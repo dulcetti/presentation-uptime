1e779f24496d028b18dd3cbcd016d931e301e04b
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-04-21 10:01:01 -0300

    feat: add interface of slides component

---
+++ src/components/slides/interfaces.ts
@@ -0,0 +1,13 @@
+import { ReactElement } from 'react';
+
+export interface ISlides {
+  background: string;
+  children: ReactElement;
+  firstLink: ILinks;
+  secondLink?: ILinks;
+}
+
+export type ILinks = {
+  label: string;
+  link: string;
+};
