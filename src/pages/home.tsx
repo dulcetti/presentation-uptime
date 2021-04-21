60b06392325c2610f2b63af957bb805347385182
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-04-21 11:05:27 -0300

    feat: finish the cover

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,18 +1,17 @@
 import Slides from '../components/slides';
 import { ILinks } from '../components/slides/interfaces';
+import Title from '../components/title';
 
 export default function Home() {
   const link: ILinks = {
-    label: 'Start',
+    label: 'Start ➡',
     link: '/the-twelfth-planet',
   };
 
   return (
     <>
       <Slides firstLink={link} background="/images/The-Twelfth-Planet.jpg">
-        <ul>
-          <li>Olar</li>
-        </ul>
+        <Title text="The history about the Twelfth Planet" />
       </Slides>
     </>
   );
