3c870b882acec96c824faf2ac79845cb71f4fe4a
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-04-23 14:41:25 -0300

    style: change styles of title and subtitle

--- src/components/title/styles.ts
+++ src/components/title/styles.ts
@@ -1,14 +1,20 @@
 import styled from 'styled-components';
 
 export const Title = styled.h1`
-  font-size: 72px;
+  font-family: 'zen-dots';
 
   &:not(.-subtitle) {
+    font-size: 4.5rem;
+    line-height: 1;
+    margin: 0 auto;
     padding: 60px 0;
     text-align: center;
+    width: 60%;
   }
 
   &.-subtitle {
+    font-size: 3rem;
+    padding: 40px;
     text-align: right;
   }
 `;
