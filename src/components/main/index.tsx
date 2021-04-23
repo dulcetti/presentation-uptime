991181e4d82bf0f8c3323b5c63f40729b0e785dd
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-04-23 14:40:27 -0300

    feat: add all routes inside main component

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,13 +1,55 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import { Home } from '../../pages';
+import {
+  Anunnaki,
+  ArkOfNoah,
+  Finish,
+  GenesisRevisited,
+  Gods,
+  Cover,
+  HumansSlaves,
+  Nibiru,
+  TheTwelfthPlanet,
+  Tiamat,
+  WhereAreTheHumans,
+} from '../../pages';
 
 export default function Main() {
   return (
     <Router>
       <Switch>
-        <Route path="/">
-          <Home />
+        <Route path="/anunnaki">
+          <Anunnaki />
+        </Route>
+        <Route exact path="/ark-of-noah">
+          <ArkOfNoah />
+        </Route>
+        <Route exact path="/finish">
+          <Finish />
+        </Route>
+        <Route exact path="/genesis-revisited">
+          <GenesisRevisited />
+        </Route>
+        <Route exact path="/gods">
+          <Gods />
+        </Route>
+        <Route exact path="/">
+          <Cover />
+        </Route>
+        <Route exact path="/human-slaves">
+          <HumansSlaves />
+        </Route>
+        <Route exact path="/nibiru">
+          <Nibiru />
+        </Route>
+        <Route path="/the-twelfth-planet">
+          <TheTwelfthPlanet />
+        </Route>
+        <Route exact path="/tiamat">
+          <Tiamat />
+        </Route>
+        <Route exact path="/where-are-the-humans">
+          <WhereAreTheHumans />
         </Route>
       </Switch>
     </Router>
