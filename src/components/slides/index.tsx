5c319d098f42f4ab24597c7dc5ff92383f6b61d6
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-04-25 20:33:58 -0300

    feat: toggle show and hide of buttons - add state to get the boolean to show or hide - add effect to execute the listener

--- src/components/slides/index.tsx
+++ src/components/slides/index.tsx
@@ -1,13 +1,21 @@
+import { useEffect, useState } from 'react';
 import { ISlides } from './interfaces';
 
 import * as S from './styles';
 
 export default function Slides({ background, children, firstLink, secondLink }: ISlides) {
+  const [talked, changeTalked] = useState<Boolean>(false);
+
+  useEffect(() => {
+    const toggleTalked = () => changeTalked(!talked);
+    document.addEventListener('mousedown', toggleTalked, false);
+  }, []);
+
   return (
     <S.SlidesWrapper bg={background}>
       <S.SlidesContent>
         {children}
-        <S.SlidesFooter>
+        <S.SlidesFooter className={talked ? '-show' : ''}>
           {firstLink && <S.SlidesLink to={firstLink.link}>{firstLink.label}</S.SlidesLink>}
           {secondLink && <S.SlidesLink to={secondLink.link}>{secondLink.label}</S.SlidesLink>}
         </S.SlidesFooter>
