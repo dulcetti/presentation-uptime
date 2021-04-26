7dd9c6ffaf34e515351ae3905ee252e94dd4a838
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-04-26 20:27:05 -0300

    feat: add finish content with image

--- src/pages/finish.tsx
+++ src/pages/finish.tsx
@@ -1,11 +1,17 @@
 import Slides from '../components/slides';
 import Title from '../components/title';
+import List from '../components/list/index';
 
 export default function Finish() {
+  const items = ['What do you think about?', 'Cheers'];
+
   return (
     <>
       <Slides background="/images/finish.jpg">
-        <Title text="We're alone here?" isSubtitle={true} />
+        <>
+          <Title text="We're alone here?" isSubtitle={true} />
+          <List items={items} image="dulcetti-meditando.png" />
+        </>
       </Slides>
     </>
   );
